a70aa26833f8eb2a55b0be4df1c155900112e35a
hoo <tbnsok40@gmail.com>
2024-04-19 01:05:53 +0900

    feat: CIDR -> host range 계산

--- main.go
+++ main.go
@@ -12,7 +12,6 @@ func main() {
 
 	fmt.Print("Input CIDR :")
 	reader := bufio.NewReader(os.Stdin)
-
 	ip, err := reader.ReadString('\n')
 	if err != nil {
 		fmt.Println("Error input", err)
@@ -20,15 +19,37 @@ func main() {
 	}
 
 	ipString := strings.TrimSpace(ip)
-	//fmt.Println("Input Ip : ", ipString)
+	network, firstIp, lastIp, broadcast := CalculateIPRange(ipString)
+	fmt.Println("네트워크 주소:", network)
+	fmt.Println("첫 번째 호스트 주소:", firstIp)
+	fmt.Println("마지막 호스트 주소:", lastIp)
+	fmt.Println("브로드캐스트 주소:", broadcast)
+}
 
-	cidr, ipNet, err := net.ParseCIDR(ipString)
+func CalculateIPRange(cidr string) (network, firstIp, lastIp, broadcast net.IP) {
+	_, ipNet, err := net.ParseCIDR(cidr)
 	if err != nil {
-		fmt.Println("Parsing Error ", err)
-		return
+		fmt.Println("CIDR 파싱 중 오류:", err)
+		return nil, nil, nil, nil
 	}
-	fmt.Println("First IP : ", cidr)
-	fmt.Println("ipNet : ", ipNet)
-	fmt.Println("ipNet.Mask : ", ipNet.Mask)
-	fmt.Println("ipNet.IP : ", ipNet.IP)
+
+	network = ipNet.IP
+	firstIP, lastIP, broadcast := make(net.IP, len(network)), make(net.IP, len(network)), make(net.IP, len(network))
+	copy(firstIP, network)
+	copy(lastIP, network)
+	copy(broadcast, network)
+
+	for i := range network {
+		network[i] &= ipNet.Mask[i]
+	}
+
+	for i := len(network) - 1; i >= 0; i-- {
+		lastIP[i] |= ^ipNet.Mask[i]
+		firstIP[i] |= 1
+	}
+
+	for i := range broadcast {
+		broadcast[i] = lastIP[i]
+	}
+	return network, firstIP, lastIP, broadcast
 }
